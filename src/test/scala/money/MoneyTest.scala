60a8118c069aa1e0b335b3f788d59a014887b43c
tofu511 <mokmok511@gmail.com>
2018-11-04 17:42:04 +0900

    add equal test

--- src/test/scala/money/MoneyTest.scala
+++ src/test/scala/money/MoneyTest.scala
@@ -6,12 +6,16 @@ class MoneyTest extends FunSuite {
 
   test("multiply 5 by 2 should be 10") {
     val ten = Dollar(5).times(2)
-    assert(10 == ten.amount)
+    assert(10 === ten.amount)
   }
 
   test("multiply 10 by 3 should be 30") {
     val thirty = Dollar(10).times(3)
-    assert(30 == thirty.amount)
+    assert(30 === thirty.amount)
+  }
+
+  test("Dollar(5) equals to Dollar(5)") {
+    assert(Dollar(5) === Dollar(5))
   }
 }
 
