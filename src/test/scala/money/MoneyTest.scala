5ba652ac62fa3c71b301b119afcafaf309be6bfb
tofu511 <mokmok511@gmail.com>
2018-11-04 23:43:58 +0900

    finish chapter 3

--- src/test/scala/money/MoneyTest.scala
+++ src/test/scala/money/MoneyTest.scala
@@ -14,8 +14,9 @@ class MoneyTest extends FunSuite {
     assert(30 === thirty.amount)
   }
 
-  test("Dollar(5) equals to Dollar(5)") {
+  test("equality") {
     assert(Dollar(5) === Dollar(5))
+    assert(Dollar(5) !== Dollar(6))
   }
 }
 
