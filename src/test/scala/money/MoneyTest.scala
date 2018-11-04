ea2cc971f1dda8df763d50b0d4c80478aa00d85b
tofu511 <mokmok511@gmail.com>
2018-11-05 00:11:26 +0900

    finish chapter 5

--- src/test/scala/money/MoneyTest.scala
+++ src/test/scala/money/MoneyTest.scala
@@ -4,7 +4,7 @@ import org.scalatest.FunSuite
 
 class MoneyTest extends FunSuite {
 
-  test("multiplication") {
+  test("dollar multiplication") {
     val five = Dollar(5)
     assert(Dollar(10) === five.times(2))
     assert(Dollar(15) === five.times(3))
@@ -14,8 +14,18 @@ class MoneyTest extends FunSuite {
     assert(Dollar(5) === Dollar(5))
     assert(Dollar(5) !== Dollar(6))
   }
+
+  test("franc multiplication") {
+    val five = Franc(5)
+    assert(Franc(10) === five.times(2))
+    assert(Franc(15) === five.times(3))
+  }
 }
 
 case class Dollar(amount: Int) {
   def times(multiplier: Int): Dollar = Dollar(amount * multiplier)
+}
+
+case class Franc(amount: Int) {
+  def times(multiplier: Int): Franc = Franc(amount * multiplier)
 }
