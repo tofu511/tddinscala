369be843c643626368e9dd6925a69d101f467b74
tofu511 <mokmok511@gmail.com>
2018-11-04 17:31:49 +0900

    fix typo

--- src/test/scala/money/MoneyTest.scala
+++ src/test/scala/money/MoneyTest.scala
@@ -5,16 +5,16 @@ import org.scalatest.FunSuite
 class MoneyTest extends FunSuite {
 
   test("multiply 5 by 2 should be 10") {
-    val ten = Doller(5).times(2)
+    val ten = Dollar(5).times(2)
     assert(10 == ten.amount)
   }
 
   test("multiply 10 by 3 should be 30") {
-    val thirty = Doller(10).times(3)
+    val thirty = Dollar(10).times(3)
     assert(30 == thirty.amount)
   }
 }
 
-case class Doller(amount: Int) {
-  def times(multiplier: Int): Doller = Doller(amount * multiplier)
+case class Dollar(amount: Int) {
+  def times(multiplier: Int): Dollar = Dollar(amount * multiplier)
 }
