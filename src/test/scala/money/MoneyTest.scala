4bdd8c2c35e8d35635c722a82b3e864131d33098
tofu511 <mokmok511@gmail.com>
2018-11-05 00:02:36 +0900

    refactor MonetTest

--- src/test/scala/money/MoneyTest.scala
+++ src/test/scala/money/MoneyTest.scala
@@ -4,14 +4,10 @@ import org.scalatest.FunSuite
 
 class MoneyTest extends FunSuite {
 
-  test("multiply 5 by 2 should be 10") {
-    val ten = Dollar(5).times(2)
-    assert(10 === ten.amount)
-  }
-
-  test("multiply 10 by 3 should be 30") {
-    val thirty = Dollar(10).times(3)
-    assert(30 === thirty.amount)
+  test("multiplication") {
+    val five = Dollar(5)
+    assert(Dollar(10) === five.times(2))
+    assert(Dollar(15) === five.times(3))
   }
 
   test("equality") {
