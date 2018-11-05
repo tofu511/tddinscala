989fb09b7e0974a65f0536e165b02be2ea1940b4
tofu511 <mokmok511@gmail.com>
2018-11-06 01:15:40 +0900

    finish chapter 9

--- src/main/scala/money/Money.scala
+++ src/main/scala/money/Money.scala
@@ -1,19 +1,21 @@
 package money
 
-sealed abstract class Money {
-  val amount: Int
+sealed trait Money {
   def times(multiplier: Int): Money
+  def currency: String
 }
 
 object Money {
-  def dollar(amount: Int): Money = Dollar(amount)
-  def franc(amount: Int): Money = Franc(amount)
+  def dollar(amount: Int): Money = Dollar(amount = amount)
+  def franc(amount: Int): Money = Franc(amount = amount)
 }
 
-case class Dollar(override val amount: Int) extends Money {
-  override def times(multiplier: Int): Money = Dollar(amount * multiplier)
+case class Dollar(private val amount: Int) extends Money {
+  override def times(multiplier: Int): Money = Money.dollar(amount = amount * multiplier)
+  override def currency: String = "USD"
 }
 
-case class Franc(override val amount: Int) extends Money {
-  override def times(multiplier: Int): Money = Franc(amount * multiplier)
+case class Franc(private val amount: Int) extends Money {
+  override def times(multiplier: Int): Money = Money.franc(amount = amount * multiplier)
+  override def currency: String = "CHF"
 }
--- src/test/scala/money/MoneyTest.scala
+++ src/test/scala/money/MoneyTest.scala
@@ -23,4 +23,9 @@ class MoneyTest extends FunSuite {
     assert(Money.franc(10) === five.times(2))
     assert(Money.franc(15) === five.times(3))
   }
+
+  test("currency") {
+    assert("USD" === Money.dollar(1).currency)
+    assert("CHF" === Money.franc(1).currency)
+  }
 }
