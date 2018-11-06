2777ec7558ed1d474993b3d407763f5bf09c1b9e
tofu511 <mokmok511@gmail.com>
2018-11-06 22:40:42 +0900

    finish chapter 11

--- src/test/scala/money/MoneyTest.scala
+++ src/test/scala/money/MoneyTest.scala
@@ -7,34 +7,21 @@ class MoneyTest extends FunSuite {
   val USD: String = "USD"
   val CHF: String = "CHF"
 
-  test("dollar multiplication") {
+  test("money multiplication") {
     implicit val currencyUnit: String = USD
     val five: Money = Money(5)
     assert(Money(10) === five.times(2))
     assert(Money(15) === five.times(3))
   }
 
-  test("dollar equality") {
+  test("money equality") {
     implicit val currencyUnit: String = USD
-    assert(Money(5) === Money(5))
-    assert(Money(5) !== Money(6))
-  }
-
-  test("franc equality") {
-    implicit val currencyUnit: String = CHF
     assert(Money(5) === Money(5))
     assert(Money(5) !== Money(6))
   }
 
   test("different currency unit equality") {
     assert(Money(5)(USD) !== Money(5)(CHF))
-  }
-
-  test("franc multiplication") {
-    implicit val currencyUnit: String = CHF
-    val five: Money = Money(5)
-    assert(Money(10) === five.times(2))
-    assert(Money(15) === five.times(3))
   }
 
   test("currency unit") {
