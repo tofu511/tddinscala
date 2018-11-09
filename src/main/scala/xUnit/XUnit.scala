c3c9455f7fb0f5605bf2b963cd12603f11f44047
tofu511 <mokmok511@gmail.com>
2018-11-09 23:21:38 +0900

    finish chapter 19

--- src/main/scala/xUnit/XUnit.scala
+++ src/main/scala/xUnit/XUnit.scala
@@ -3,27 +3,45 @@ package xUnit
 object XUnit {
   def main(args: Array[String]): Unit = {
     TestCaseTest("testRunning").run()
+    TestCaseTest("testSetUp").run()
   }
 }
 
 case class WasRun(testName: String) extends TestCase {
+  var wasSetUp: Boolean = false
   var wasRun: Boolean = false
-  def run(): Unit = this.testMethod()
+  // 動的に呼び出したい
+  def run(): Unit = testName match {
+    case "testMethod" => testMethod()
+    case "testSetUp" => setUp()
+  }
   def testMethod() : Unit = wasRun = true
+  def setUp(): Unit = wasSetUp = true
 
   override val name: String = testName
 }
 
 case class TestCaseTest(testCase: String) extends TestCase {
-  def testRunning(): Unit= {
+  // 動的にテストを呼び出せるようになったらWasRunインスタンスを一つにまとめたい
+  def testRunning(): Unit = {
     val test = WasRun("testMethod")
-    assert(!test.wasRun)
     test.run()
     assert(test.wasRun)
   }
 
+  def testSetUp(): Unit = {
+    val test = WasRun("testSetUp")
+    test.run()
+    assert(test.wasSetUp)
+  }
+
   override val name: String = testCase
-  override def run(): Unit = testRunning()
+
+  // 動的に呼び出したい
+  override def run(): Unit = testCase match {
+    case "testRunning" => testRunning()
+    case "testSetUp" => testSetUp()
+  }
 }
 
 trait TestCase {
